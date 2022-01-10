6f81bf0c98793c1c34598fbbdc57e8f3ebed10a8
JintekiWarrior <jintekiwarrior@gmail.com>
2022-01-10 12:43:55 -0500

    created the quiz create component and tested to make sure it shows, also created the api request for quiz create

---
+++ src/api/quiz.js
@@ -0,0 +1,20 @@
+/*** create quiz api request routes ***/ 
+import apiUrl from "../apiConfig";
+import axois from 'axios'
+import axios from "axios";
+
+export const createQuiz = (user, title, description) => {
+    return axios({
+        method: 'POST',
+        url: apiUrl + '/quiz',
+        headers: {
+            'Authorization': `Bearer ${user.token}`
+        },
+        data: {
+            quiz: {
+                title: title,
+                description: description
+            }
+        }
+    })
+} 
