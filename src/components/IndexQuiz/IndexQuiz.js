3938f5e95fc85685b6542b073c59d76d1203c448
JintekiWarrior <jintekiwarrior@gmail.com>
2022-02-02 10:53:35 -0500

    Add a show quizzes component that allows the user to see the quizzes

--- src/components/IndexQuiz/IndexQuiz.js
+++ src/components/IndexQuiz/IndexQuiz.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { indexQuiz } from './../../api/quiz'
 
+// Index Quizzes component
 const IndexQuiz = () => {
+    const [quizzes, setQuizzes] = useState([])
+
+    // Api request to show the quiz
+    useEffect(() => {
+        const show = async () => {
+            try {
+                const res = await indexQuiz()
+                setQuizzes(res.data.quiz)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        show()
+    }, []) 
+
     return (
         <div>
-            Index Quizzes
+            {quizzes.map(quiz => (
+                <div key={quiz._id}>
+                    <h4>{quiz.title}</h4>
+                    <p>{quiz.description}</p>
+                </div>
+            ))}
         </div>
     )
 }
