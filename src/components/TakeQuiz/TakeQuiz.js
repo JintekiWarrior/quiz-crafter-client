beb9b7719cc971e33aada455fcb9b0c4a510644e
JintekiWarrior <jintekiwarrior@gmail.com>
2022-04-25 12:51:34 -0400

    Add Take Quiz component and functionality - User can take a quiz - Score is returned after the quiz is submitted

--- src/components/TakeQuiz/TakeQuiz.js
+++ src/components/TakeQuiz/TakeQuiz.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react'
 import { showQuiz } from './../../api/quiz'
 import { useParams } from 'react-router-dom'
 
+// Component that allows the user to take a quiz
 const TakeQuiz = ({ user }) => {
   const [quiz, setQuiz] = useState([])
   const [questions, setQuestions] = useState([])
+
+  const [answers, setAnswers] = useState({})
 
   let { id } = useParams()
   // make api call to quiz
@@ -22,8 +25,27 @@ const TakeQuiz = ({ user }) => {
     show()
   }, [])
 
-  console.log(quiz)
-  console.log(questions)
+  const handleChange = event => {
+    const { name, value } = event.target
+    setAnswers(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+  const onSubmitQuiz = event => {
+    event.preventDefault()
+    console.log("answers", answers)
+    const answerLength = Object.keys(answers).length
+    let rightAnswerCount = 0
+    Object.entries(answers).forEach(
+      ([key, value]) => {
+        if (value === "rightAnswer") rightAnswerCount++
+      }
+    )
+    console.log(Math.trunc((rightAnswerCount / answerLength) * 100))
+    return Math.trunc((rightAnswerCount / answerLength) * 100)
+  }
 
   return (
     <div>
@@ -31,19 +53,20 @@ const TakeQuiz = ({ user }) => {
         <h3 className='quiz__heading'>{quiz.title}</h3>
         <p className='quiz__description'>{quiz.description}</p>
       </div>
-      {questions.map(item => (
+      <form id='take-quiz-form' onSubmit={onSubmitQuiz}>
+        {questions.map(item => (
           <div className='quiz__container' key={item._id}>
             <h4 className='quiz__heading'>{item.question}</h4>
-            <form>
-              <input type='radio' id='rightAnswer' name='quiz_question' />
-              <label for='rightAnswer'>{item.rightAnswer}</label><br/>
-              <input type='radio' id='wrongAnswerOne' name='quiz_question' />
-              <label for='wrongAnswerOne'>{item.wrongAnswerOne}</label><br/>
-              <input type='radio' id='wrongAnswerTwo' name='quiz_question' />
-              <label for='wrongAnswerTwo'>{item.wrongAnswerTwo}</label><br/>
-            </form>
+            <input type='radio' id='rightAnswer' name={item._id} value='rightAnswer' onChange={handleChange} />
+            <label htmlFor='rightAnswer'>{item.rightAnswer}</label><br/>
+            <input type='radio' id='wrongAnswerOne' name={item._id} value='wrongAnswerOne' onChange={handleChange} />
+            <label htmlFor='wrongAnswerOne'>{item.wrongAnswerOne}</label><br/>
+            <input type='radio' id='wrongAnswerTwo' name={item._id} value='wrongAnswerTwo' onChange={handleChange} />
+            <label htmlFor='wrongAnswerTwo'>{item.wrongAnswerTwo}</label><br/>
           </div>
         ))}
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   )
 }
