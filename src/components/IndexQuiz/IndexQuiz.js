f64a36f5c2a2279393b0998935025ef60c2ae294
JintekiWarrior <jintekiwarrior@gmail.com>
2022-04-29 16:27:22 -0400

    Add style to show and index quiz components

--- src/components/IndexQuiz/IndexQuiz.js
+++ src/components/IndexQuiz/IndexQuiz.js
@@ -1,46 +1,44 @@
-import './Index.scss'
-import './../../css/quiz.scss'
-import React, { useState, useEffect } from 'react'
-import { indexQuiz } from './../../api/quiz'
-import { Link } from 'react-router-dom'
+import "./Index.scss";
+import "./../../css/quiz.scss";
+import React, { useState, useEffect } from "react";
+import { indexQuiz } from "./../../api/quiz";
+import { Link } from "react-router-dom";
 
 // Index Quizzes component
 const IndexQuiz = () => {
-    const [quizzes, setQuizzes] = useState([])
+  const [quizzes, setQuizzes] = useState([]);
 
-    // Api request to show the quizzes
-    useEffect(() => {
-        const showQuizzes = async () => {
-            try {
-                const res = await indexQuiz()
-                setQuizzes(res.data.quiz)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        showQuizzes()
-    }, []) 
+  // Api request to show the quizzes
+  useEffect(() => {
+    const showQuizzes = async () => {
+      try {
+        const res = await indexQuiz();
+        setQuizzes(res.data.quiz);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    showQuizzes();
+  }, []);
 
-    return (
-        <div className='quiz'>
-            {quizzes.map(quiz => (
-                <div className='quiz__container' key={quiz._id}>
-                    <h4 className='quiz__heading'>{quiz.title}</h4>
-                    <p className='quiz__description'>{quiz.description}</p>
-                    <Link
-                        className='quiz__link'
-                        to={`/show-quizzes/${quiz._id}`}
-                    >
-                        Go To 
-                    </Link>
-                    <Link
-                        className='quiz__link'
-                        to={`/take-quiz/${quiz._id}`}
-                    >Take Quiz</Link>
-                </div>
-            ))}
+  return (
+    <div className="quiz">
+      {quizzes.map((quiz) => (
+        <div className="quiz__container" key={quiz._id}>
+          <h4 className="quiz__heading">{quiz.title}</h4>
+          <p className="quiz__description">{quiz.description}</p>
+          <div className="quiz__button-box">
+            <Link className="quiz__btn" to={`/show-quizzes/${quiz._id}`}>
+              Go To
+            </Link>
+            <Link className="quiz__btn" to={`/take-quiz/${quiz._id}`}>
+              Take Quiz
+            </Link>
+          </div>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default IndexQuiz
+export default IndexQuiz;
--- src/components/ShowQuiz/ShowQuiz.js
+++ src/components/ShowQuiz/ShowQuiz.js
@@ -1,71 +1,78 @@
-import React, { useState, useEffect } from 'react'
-import { showQuiz, deleteQuiz } from './../../api/quiz'
-import { useParams, Link } from 'react-router-dom'
-import CreateQuestion from '../CreateQuestion/CreateQuestion'
-import ShowQuestions from '../ShowQuestions/ShowQuestions'
+import React, { useState, useEffect } from "react";
+import { showQuiz, deleteQuiz } from "./../../api/quiz";
+import { useParams, Link } from "react-router-dom";
+import CreateQuestion from "../CreateQuestion/CreateQuestion";
+import ShowQuestions from "../ShowQuestions/ShowQuestions";
 
 const ShowQuiz = ({ user }) => {
-    const [quiz, setQuiz] = useState([])
-    const [showQuestionForm, setShowQuestionForm] = useState(false)
-    const [showQuestions, setShowQuestions] = useState([])
-    const [isListUpdated, setIsListUpdated] = useState(0)
+  const [quiz, setQuiz] = useState([]);
+  const [showQuestionForm, setShowQuestionForm] = useState(false);
+  const [showQuestions, setShowQuestions] = useState([]);
+  const [isListUpdated, setIsListUpdated] = useState(0);
 
-    let { id } = useParams()
+  let { id } = useParams();
 
-    // Api request to show a quiz
-    useEffect(() => {
-        const show = async () => {
-            try {
-                const res = await showQuiz(user, id)
-                setQuiz(res.data.quiz)
-                setShowQuestions(res.data.quiz.questions)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        show()
-    }, [isListUpdated])
+  // Api request to show a quiz
+  useEffect(() => {
+    const show = async () => {
+      try {
+        const res = await showQuiz(user, id);
+        setQuiz(res.data.quiz);
+        setShowQuestions(res.data.quiz.questions);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    show();
+  }, [isListUpdated]);
 
-    // Api request to delete a quiz
-    const onDeleteQuiz = event => {
-        event.preventDefault()
-        const destroy = async () => {
-            try {
-                const res = await deleteQuiz(user, id)
-                console.log(res + "data deleted")
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        destroy()
-    }
+  // Api request to delete a quiz
+  const onDeleteQuiz = (event) => {
+    event.preventDefault();
+    const destroy = async () => {
+      try {
+        const res = await deleteQuiz(user, id);
+        console.log(res + "data deleted");
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    destroy();
+  };
 
-    // allows the user to add multiple question forms 
-    const onAddQuestion = event => {
-        !showQuestionForm ? setShowQuestionForm(true) : setShowQuestionForm(false)
-    }
+  // allows the user to add multiple question forms
+  const onAddQuestion = (event) => {
+    !showQuestionForm ? setShowQuestionForm(true) : setShowQuestionForm(false);
+  };
 
-    return (
-        <div>
-            <div className='quiz__container'>
-                <h3 className='quiz__heading'>{quiz.title}</h3>
-                <p className='quiz__description'>{quiz.description}</p>
-                <div className='quiz__button-container'>
-                    <Link className='quiz__link' to={`/update-quiz/${id}`}>Update</Link>
-                    <button className='quiz__button' onClick={onDeleteQuiz}>Delete</button>
-                    <button className='quiz__button' onClick={onAddQuestion}>Add Question</button>
-                </div>
-            </div>
-            { showQuestionForm ? <CreateQuestion 
-                user={user} 
-                quizId={id} 
-                setIsListUpdated={setIsListUpdated}
-                isListUpdated={isListUpdated} 
-                /> : null 
-            }
-            <ShowQuestions showQuestions={showQuestions} quizId={id} user={user} />
+  return (
+    <div className="quiz">
+      <div className="quiz__container">
+        <h3 className="quiz__heading">{quiz.title}</h3>
+        <p className="quiz__description">{quiz.description}</p>
+        <div className="quiz__button-box">
+          <Link className="quiz__btn" to={`/update-quiz/${id}`}>
+            Update
+          </Link>
+          <a className="quiz__btn" onClick={onAddQuestion}>
+            Add Question
+          </a>
+          <a className="quiz__btn" onClick={onDeleteQuiz}>
+            Delete
+          </a>
         </div>
-    )
-}
+      </div>
+      {showQuestionForm ? (
+        <CreateQuestion
+          user={user}
+          quizId={id}
+          setIsListUpdated={setIsListUpdated}
+          isListUpdated={isListUpdated}
+        />
+      ) : null}
+      <ShowQuestions showQuestions={showQuestions} quizId={id} user={user} />
+    </div>
+  );
+};
 
-export default ShowQuiz
+export default ShowQuiz;
